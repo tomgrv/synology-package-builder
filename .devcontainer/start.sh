f33801531d093a6a1b23004cc39de1a819babcb4
Thomas G <1809566+tomgrv@users.noreply.github.com>
2025-02-12 18:55:07 +0000

    ci: 👷 update devcontainer config

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -3,6 +3,7 @@
 ### Make sure the workspace folder is owned by the user
 git config --global --add safe.directory ${containerWorkspaceFolder:-.}
 
-### Self update
-echo "Self updating devcontainer..."
-npx --yes $(sed -e 's://.*$::g' .devcontainer/devcontainer.json | npx --yes jqn '.name' | tr -d "'[]:,")  -a
+### Define gpg configuration
+if [ -z "$CODESPACES" ]; then
+    git config --global gpg.program gpg2
+fi
