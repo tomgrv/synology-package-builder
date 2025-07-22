655ed375ed89bfc7ce80e5830e8a5fded0d24077
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-07-22 19:58:04 +0900

    Delete ChangePanelSize/config.js

--- ChangePanelSize/config.js
+++
@@ -1,9 +0,0 @@
-{
-  "package": "changepanelsize",
-  "arch": "x86_64",
-  "version": "1.0.0",
-  "description": "Storage panel size configuration tool",
-  "displayname_csy": "Change Panel Size",
-  "thirdparty": "yes",
-  "beta": "false"
-}
